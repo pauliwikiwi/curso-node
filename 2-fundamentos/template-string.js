b32ae25c725fa00f1b7406d97f6d48db0fbf79b6
iwiikiwii <pblazquezwnuk@gmail.com>
2022-06-21 19:06:25 +0200

    ejemplos de template string

---
+++ 2-fundamentos/template-string.js
@@ -0,0 +1,17 @@
+const nombre = 'Mickey';
+const girlfriend = 'Minnie';
+
+
+const pareja = nombre + ' ' + girlfriend;
+const template = `${nombre} ${girlfriend}`;
+
+if (pareja === template){
+    console.log(true);
+}
+
+const html = `
+<h1>Hola</h1>
+<p>Mundo</p>
+`;
+
+console.log(html)
