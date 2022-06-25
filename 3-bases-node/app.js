5a24d7804e0953d90c352c32aebb72abf885d2dd
iwiikiwii <pblazquezwnuk@gmail.com>
2022-06-25 14:54:00 +0200

    Añadido package.json desde consola e instalacion del primer paquete

--- 3-bases-node/app.js
+++ 3-bases-node/app.js
@@ -1,9 +1,16 @@
 const {crearArchivo} = require('./helpers/multiplicar')
 
 console.clear();
-const base = 8;
+
+const [,,arg3 = '--base=7'] = process.argv;
+const [,base] = arg3.split('=');
+console.log(base);
+
+/*const base = 8;*/
 
 
-crearArchivo(base)
-    .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
-    .catch(err => console.log(err));
+
+
+    crearArchivo(base)
+        .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+        .catch(err => console.log(err));
